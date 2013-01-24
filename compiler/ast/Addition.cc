e17a4c7ac37bbe91ee1eacf47f02fb1479013773
Heinrich Schmidt <kt003@ktrask.de>
2013-01-24 17:32:49 +0100

    Addition added

--- compiler/ast/Addition.cc
+++ compiler/ast/Addition.cc
@@ -15,3 +15,18 @@ std::string Addition::explain(int ind)
   return expl.str();
 }
 
+void Addition::generate(CodeContainer *code, SymbolTable *table)
+{
+	addend->generate(code, table);
+	code->addStackPush(code->exprResultAddr);
+	augend->generate(code, table);
+	code->addStackPop(code->tempAddr);
+	// change stack pointer by +1
+	code->push_back(code->clearAddr);
+	code->push_back(code->clearAddr);
+	code->push_back(code->clearAddr);
+	code->push_back(code->tempAddr); // load *tempAddr to akk
+	code->push_back(code->clearAddr); // (0-x)
+	code->push_back(1); // skip
+	code->push_back(code->exprResultAddr); // (*exprResultAddr) + x
+}
--- compiler/ast/Addition.h
+++ compiler/ast/Addition.h
@@ -12,6 +12,7 @@ class Addition: public Expression
 	Expression * addend;
 	Addition(int line, Expression *e1, Expression *e2) : Expression(line) { augend = e1; addend = e2;}
 	std::string explain(int ind);
+	void generate(CodeContainer *code, SymbolTable *table);
 };
 
 #endif //ADDITION_H

b0f523b95d3575a9928210a0028d2dce62b1c76a
Heinrich Schmidt <kt003@ktrask.de>
2013-01-29 18:06:32 +0100

    indirection assignment fixed

--- compiler/ast/IndirectionAssignment.cc
+++ compiler/ast/IndirectionAssignment.cc
@@ -18,42 +18,65 @@ std::string IndirectionAssignment::explain(int ind)
 
 void IndirectionAssignment::generate(CodeContainer* code, SymbolTable* table)
 {
+	code->addComment( "IndirectionAssignment start" );
 	augend->generate(code, table); // evaluated the expression on the right side of the =
 	code->addStackPush(code->exprResultAddr); // pushes exprResult to stack
 
 	pointer_->generate(code, table); // evalutes the pointer address
 	code->addStackPop(code->tempAddr); // pops the right expr result to tempAddr
 	code->addClearAkk();
+	code->addComment( "IndirectionAssignment assignment" );
 	int localAddr = code->address();
-	code->push_back(localAddr + 15);
-	code->push_back(localAddr + 15);
-	code->push_back(localAddr + 15);
+	code->push_back(localAddr + 30);
+	code->push_back(localAddr + 30);
+	code->push_back(localAddr + 30);
 	code->push_back(code->exprResultAddr);
-	code->push_back(localAddr + 15);
-	code->push_back(localAddr + 16);
-	code->push_back(localAddr + 16);
-	code->push_back(localAddr + 16);
+	code->push_back( code->clearAddr ); // 0 - *exprResultAddr
+	code->push_back( code->clearAddr ); //skipped or zero
+	code->push_back(localAddr + 30);
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back(localAddr + 31);
+	code->push_back(localAddr + 31);
+	code->push_back(localAddr + 31);
 	code->push_back(code->exprResultAddr);
-	code->push_back(localAddr + 16);
-	code->push_back(localAddr + 17);
-	code->push_back(localAddr + 17);
-	code->push_back(localAddr + 17);
+	code->push_back( code->clearAddr ); // 0 - *exprResultAddr
+	code->push_back( code->clearAddr ); //skipped or zero
+	code->push_back(localAddr + 31);
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back(localAddr + 32);
+	code->push_back(localAddr + 32);
+	code->push_back(localAddr + 32);
 	code->push_back(code->exprResultAddr);
-	code->push_back(localAddr + 17);
-	code->push_back(0); // get overwritten to clear stackaddress
-	code->push_back(0);
-	code->push_back(0);
+	code->push_back( code->clearAddr ); // 0 - *exprResultAddr
+	code->push_back( code->clearAddr ); //skipped or zero
+	code->push_back(localAddr + 32);
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( 0xeeee ); // get overwritten to clear stackaddress
+	code->push_back( 0xeeee );
+	code->push_back( 0xeeee );
 	localAddr = code->address();
-	code->push_back(localAddr + 8);
-	code->push_back(localAddr + 8);
-	code->push_back(localAddr + 8);
-	code->push_back(code->exprResultAddr);
-	code->push_back(localAddr + 8);
+	code->push_back(localAddr + 14);
+	code->push_back(localAddr + 14);
+	code->push_back(localAddr + 14);
+	code->push_back(code->exprResultAddr); // load *exprResultAddr
+	code->push_back( code->clearAddr ); // 0 - *exprResultAddr
+	code->push_back( code->clearAddr ); // skipped or zero
+	code->push_back(localAddr + 14);  // 0 - (0 - *exprResultAddr)
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
+	code->push_back( code->clearAddr ); //clear *clearAddr and akk
 	code->push_back(code->tempAddr); // load result of the right expression
 	code->push_back(code->clearAddr); // store -(*addr) in akk and borrow
 	code->push_back(1); // skip, use the 1 later
 	//int oneAddr = localAddr + 7;
-	code->push_back(0); // store *addr at the pointer position
+	code->push_back( 0xeeee ); // store *addr at the pointer position
 
 	
 }

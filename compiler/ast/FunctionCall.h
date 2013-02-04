b64cf923af3463d1a7fa03d8307108a4ff399c15
Drahflow <drahflow@gmx.de>
2013-02-04 22:19:37 +0100

    Compiler warning removed

--- compiler/ast/FunctionCall.h
+++ compiler/ast/FunctionCall.h
@@ -10,8 +10,8 @@ class FunctionCall: public Expression
 {
 	public:
 	std::vector<Expression*>* values_;
-	char* name_;
-	FunctionCall(int line, char* name, std::vector<Expression *>* values) : Expression(line) { values_ = values; name_ = name;}
+	const char* name_;
+	FunctionCall(int line, const char* name, std::vector<Expression *>* values) : Expression(line) { values_ = values; name_ = name;}
 	std::string explain(int ind);
 	void generate(CodeContainer*, SymbolTable*);
 };
